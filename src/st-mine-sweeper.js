7cd67b436c221730829a8cc0067aafeb92705232
sasaafanasienka <mark0182@tut.by>
2021-09-12 15:47:00 +0300

    minesweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,15 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  return matrix.map((row, rowNum) => {
+    return row.map((el, colNum) => {
+      const leftEdge = colNum - 1 < 0 ? 0 : colNum - 1
+      const rightEdge = colNum + 2 > 3 ? 3 : colNum + 2
+      const topRow = matrix[rowNum-1] ? matrix[rowNum-1].slice(leftEdge,rightEdge).filter(el=>el).length : 0
+      const curRow = [ matrix[rowNum][colNum-1] , matrix[rowNum][colNum + 1] ].filter(el=>el).length
+      const botRow = matrix[rowNum+1] ? matrix[rowNum+1].slice(leftEdge,rightEdge).filter(el=>el).length : 0
+      return topRow + curRow + botRow
+    })
+  })
 }
