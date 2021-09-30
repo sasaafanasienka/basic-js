f721c1a6a8ad3ff68062e7f9cf586f65e85b4514
sasaafanasienka <you@example.comafanasienka@yandex.by>
2021-09-30 14:33:06 +0300

    vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+// import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,12 +20,72 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(method) {
+    this.method = method
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, keyword) {
+
+    if ( !string || !keyword ) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    const encryptSymbol = (...args) => {
+      const symbols = [...args].map(el => el.toUpperCase())
+      const res = (symbols[0].charCodeAt() + symbols[1].charCodeAt() - 130) % 26
+      return String.fromCharCode(res + 65)
+    }
+  
+    const klength = keyword.length
+    const str = string.toUpperCase()
+    const key = Array(Math.ceil(str.length / klength)).fill(keyword.toUpperCase()).join('')
+    let j = 0;
+    let res = ''
+    for ( let i = 0; i < str.length; i++ ) {
+      if ( /[A-Z]/.test(str[i]) ) {
+        res = res.concat(encryptSymbol(str[i], key[j]))
+        j++
+      } else {
+        res = res.concat(str[i])
+      }
+    }
+    return this.method === false ? res.split('').reverse().join('') : res
   }
+
+  decrypt(string, keyword) {
+
+    if ( !string || !keyword ) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    const decryptSymbol = (...args) => {
+      const symbols = [...args].map(el => el.toUpperCase())
+      const res = (symbols[0].charCodeAt() - symbols[1].charCodeAt() + 130) % 26
+      return String.fromCharCode(res + 65)
+    }
+  
+    const klength = keyword.length
+    const str = string.toUpperCase()
+    const key = Array(Math.ceil(str.length / klength)).fill(keyword.toUpperCase()).join('')
+    let j = 0;
+    let res = ''
+    for ( let i = 0; i < str.length; i++ ) {
+      if ( /[A-Z]/.test(str[i]) ) {
+        res = res.concat(decryptSymbol(str[i], key[j]))
+        j++
+      } else {
+        res = res.concat(str[i])
+      }
+    }
+    return this.method === false ? res.split('').reverse().join('') : res
+  }
+
 }
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+console.log(directMachine.encrypt('attack at dawn!', 'alphonse')) // 'AEIHQX SX DLLU!'
+console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')) // 'ATTACK AT DAWN!'
+console.log(reverseMachine.encrypt('attack at dawn!', 'alphonse')) // '!ULLD XS XQHIEA'
+console.log(reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')) // '!NWAD TA KCATTA'
